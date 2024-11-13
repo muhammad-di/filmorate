d54266273ffa8f0c3db012fb935450c3306443cb
Muhammad <dzhabrailov3.muhammad@yandex.ru>
2024-11-13 10:39:25 +0300

    fix: создать сущность Film расставить к ней нужные аннотации из lombok

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,21 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@Builder
+@EqualsAndHashCode(of = "id")
 public class Film {
+    private final UUID id;
+    private String description;
+    private final LocalDate releaseDate;
+    private final long duration;
 }
